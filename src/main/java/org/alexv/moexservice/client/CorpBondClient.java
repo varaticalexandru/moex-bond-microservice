c4d5bccae50736ae4ddceb0792b7220173c586f5
alex <varaticalexandru1@gmail.com>
2024-02-21 22:14:56 +0200

    upd endpoints add Resilience4j: retry, circuitbreaker

--- src/main/java/org/alexv/moexservice/client/CorpBondClient.java
+++ src/main/java/org/alexv/moexservice/client/CorpBondClient.java
@@ -1,9 +1,13 @@
 package org.alexv.moexservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "corporate-bonds", url = "${moex.bonds.corporate.url}", configuration = FeignConfig.class)
+@CircuitBreaker(name = "bond-breaker")
+@Retry(name = "bond-retry")
 public interface CorpBondClient {
     @GetMapping
     String getBondsFromMoex();
--- src/main/java/org/alexv/moexservice/client/GovBondClient.java
+++ src/main/java/org/alexv/moexservice/client/GovBondClient.java
@@ -1,9 +1,13 @@
 package org.alexv.moexservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "gov-bonds", url = "${moex.bonds.government.url}", configuration = FeignConfig.class)
+@CircuitBreaker(name = "bond-breaker")
+@Retry(name = "bond-retry")
 public interface GovBondClient {
     @GetMapping
     String getBondsFromMoex();
